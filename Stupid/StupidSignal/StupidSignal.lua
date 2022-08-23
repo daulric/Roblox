6091ead802ddc5d7b87bda9ca520aa83416edfb7
Ulric <57965846+daulric@users.noreply.github.com>
2022-08-23 00:00:00 +0000

    made updates

--- Stupid/StupidSignal/StupidSignal.lua
+++ Stupid/StupidSignal/StupidSignal.lua
@@ -1,21 +1,21 @@
 local module = {}
-local RunService = game:GetService("RunService")
+local Signal = {}
 
 function module.new(name)
-    local self = {}
+    local self = setmetatable(module, Signal)
     self.args = {}
     self.BindEvent = Instance.new("BindableEvent")
     self.BindEvent.Name = name
     return self
 end
 
-function module:Fire(...)
+function Signal:Fire(...)
     table.insert(self.args, ...)
     table.pack(self.args)
     self.BindEvent:Fire(self.args)
 end
 
-function module.OnEvent(handler)
+function Signal.OnEvent(handler)
     if not (type(handler) == "function") then
         print("this is not a function")
     end
